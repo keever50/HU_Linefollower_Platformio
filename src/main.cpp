5e21db8cb1ad9184aa0f263b6c41bb6ba796943e
Kevin Witteveen <kevinwit1999@gmail.com>
2023-10-25 19:52:10 +0200

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,67 @@
 #include <Arduino.h>
+#include <sniffer.h>
+#include <wheels.h>
+#include <time.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//#define MAIN_DEBUG
+
+unsigned long get_performance()
+{
+  static unsigned long previous = 0;
+  unsigned long now = micros();
+  unsigned long lasted = now - previous;
+  previous = now;
+  return lasted;
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(250000);
+  wheels_initialize();
+
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  //Reset terminal position
+  Serial.print("\e[0J");
+  Serial.print("\e[H");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("uS per cycle: ");
+  Serial.println(get_performance());
+
+  char IR_sensors[SNIFFER_PINS];
+  sniffer_read(IR_sensors);
+
+  #ifdef MAIN_DEBUG
+    Serial.print("[");
+    for(int i=0;i<SNIFFER_PINS;i++)
+    {
+      if(IR_sensors[i])
+      {
+        Serial.print(" # ");
+      }else{
+        Serial.print("   ");
+      }
+    }
+    Serial.print("] ");
+  #endif
+
+  char lost;
+  float steering = sniffer_get_steering(IR_sensors, &lost);
+  
+  #ifdef MAIN_DEBUG
+    if(lost)
+    {
+      Serial.print("  LOST  \n\r");
+    }else
+    {
+      Serial.print("TRACKING\n\r");
+    }
+    Serial.print("Steering: ");
+    Serial.println(steering);
+  #endif 
+
+
+  wheels_move(0.08, steering*1.4);
+ // delay(1);
 }
