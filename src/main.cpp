96f3582fbd452148eeea5494db4753720e48588a
Kevin Witteveen <kevin.witteveen@student.hu.nl>
2023-11-02 15:46:31 +0100

    tests

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 #include <wheels.h>
 #include <led.h>
 #include <Adafruit_NeoPixel.h>
-
+#include <Servo.h>
 #include <behavior.cpp>
 
 #define LED_MAX_BRIGHTNESS  100
 
-
+Servo arm;
 
 void setup()
 {
@@ -17,7 +17,8 @@ void setup()
     wheels_initialize();
     
     pinMode(7, OUTPUT);
-
+    
+    pinMode(12, OUTPUT);
     //strip_pointer = leds_get_strip();
 }
 
@@ -26,7 +27,24 @@ char siren_half = 0;
 unsigned long delta = 0;
 void loop()
 {
+    static unsigned long Th;
+    static unsigned long next_angle;
+    if(millis()>=next_angle)
+    {
+        static char toggle;
+        toggle=!toggle;
+        next_angle = millis()+2000;
+        Th=500+(2000*toggle);
+    }
 
+    static unsigned long next_servo;
+    if(millis()>=next_servo)
+    {
+        next_servo = millis()+20;
+        digitalWrite(12,HIGH);
+        delayMicroseconds(Th);
+        digitalWrite(12,LOW);
+    }
 
     
     unsigned long delta_start;
@@ -36,44 +54,6 @@ void loop()
 
     delta = micros()-delta_start;
 
-    //SIREN//
-
-    // static unsigned long siren_switch;
-    // if(millis()>=siren_switch)
-    // {
-    //     siren_switch=millis()+750;
-    //     siren_tone=!siren_tone;
-    // }
-
-    // static unsigned long siren_delay;
-    // if(micros()>=siren_delay)
-    // {
-    //     switch(siren_tone)
-    //     {
-    //         case 0:
-    //         {
-                
-    //             digitalWrite(7,HIGH);
-                
-    //             digitalWrite(7,LOW);
-    //             break;
-    //         }
-    //         case 1:
-    //         {
-    //             siren_half=!siren_half;
-    //             if(siren_half)
-    //             {
-                    
-    //                 digitalWrite(7,HIGH);
-                    
-    //                 digitalWrite(7,LOW);
-    //             }
-
-    //             break;
-    //         }
-    //     }
-    //     siren_delay=micros()+1000-(delta/1000);
-    // }
 
 
 }
