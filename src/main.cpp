0795fec165eda8cdb8a60b9da7e2a1118a75deb9
Kevin Witteveen <kevinwit1999@gmail.com>
2023-11-08 15:21:27 +0100

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,22 @@
 #include <sniffer.h>
 #include <wheels.h>
 #include <led.h>
-#include <Adafruit_NeoPixel.h>
-#include <Servo.h>
 #include <behavior.cpp>
 
 #define LED_MAX_BRIGHTNESS  100
 
-Servo arm;
 
 void setup()
 {
-    
     Serial.begin(250000);
     wheels_initialize();
     
     pinMode(7, OUTPUT);
-    
     pinMode(12, OUTPUT);
-    //strip_pointer = leds_get_strip();
+
 }
 
-char siren_tone = 0;
-char siren_half = 0;
-unsigned long delta = 0;
 void loop()
 {
-
-
-    
-    unsigned long delta_start;
-    delta_start = micros();
-
     behavior_update();
-
-    delta = micros()-delta_start;
-
-
-
 }
